42d239d2caca8930516515437fa1abce99788540
Christian Carsin <contact@muldev.dev>
2019-11-22 16:11:03 +0100

    revision

--- modelos/Aplicacion.py
+++ modelos/Aplicacion.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api
 class Aplicacion(models.Model):
 	_name = 'tech.aplicaciones'
 	nombre = fields.Char('Nombre app', required=True)
-    cliente = fields.Char('Cliente app', required=True)
-    tipoApp = fields.Char('Tipo app', required=True)
-    costeDesarrollo = fields.Float('Coste desarrollo', required=True)
+	cliente = fields.Char('Cliente app', required=True)
+	tipoApp = fields.Char('Tipo app', required=True)
+	costeDesarrollo = fields.Float('Coste desarrollo', required=True)
