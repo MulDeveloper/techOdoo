20506ea0df8fe6504c5b1f29f0539dc3c3b72012
Christian Carsin <contact@muldev.dev>
2020-01-20 19:01:40 +0100

    Operaciones1

--- modelos/Movil.py
+++ modelos/Movil.py
@@ -3,15 +3,14 @@ from odoo import models, fields, api
 class Movil(models.Model):
 	_name = 'tech.moviles'
 	modelo = fields.Char('Modelo movil', required=True)
+	cantidad = fields.Integer('Cantidad terminales', required=True)
 	marca = fields.Char('Marca', required=True)
 	pulgadas = fields.Float('Pulgadas', required=True)
 	precio = fields.Float('Precio', required=True)
+	iva = fields.Float('IVA', required=True)
 
 
-@api.multi
-def limpiar_campos(self):
-	self.modelo = ""
-	self.marca = ""
-	self.pulgadas = ""
-	self.precio = ""
-	return True
+	@api.one
+	@api.depends('precio', 'cantidad', 'iva')
+	def _total(self):
+		self.total = self.precio * self.cantidad * self.iva
